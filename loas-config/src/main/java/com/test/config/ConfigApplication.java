3f26e62dac697e2d0804800a48b3df07372f9f17
xdcloas113 <loasloas13>
2018-08-27 10:54:15 +0800

    first commit

---
+++ loas-config/src/main/java/com/test/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.test.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * Created scs on 2018-08-27.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+@RefreshScope
+public class ConfigApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
---
+++ loas-eureka/src/main/java/com/test/register/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.test.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created scs on 2018-08-27.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+
+}
